bfa9157d9541f1fa2421017f82f0bd2e143d9340
DeanC <>
2019-01-02 12:08:38 -0500

    Document graphql playground login, _checkAuth and _currentUser instructions.

--- README.md
+++ README.md
@@ -186,6 +186,25 @@ Then create the seed records with:
 
 
 
+## GraphQL Playground login notes
+To make login queries work in the graphql playground (with these starter secrets):
+- use '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92' for the login query's password
+
+  This is the client-side crypto-hashed  for the '123456' password.
+
+- After that, to make _checkAuth and _currentUser work in the playground, set these headers:
+
+  <code>
+  {
+    
+      "x-connector-auth-request-type": "LOCAL_STORAGE",
+
+      "x-connector-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VybmFtZSI6InJpYzAiLCJlbWFpbCI6ImFkbWluQHRlc3QuaXQiLCJyb2xlcyI6WyJBRE1JTiIsIlVTRVIiXSwicGVybWlzc2lvbnMiOlsicmVhZDpjb21tZW50cyIsInJlYWQ6cHJvZmlsZSIsInJlYWQ6Y29tbWVudHMiXX0sImlhdCI6MTU0NjQ0MjgxNSwiZXhwIjoxNTQ2NDQzMTc1fQ.EEcCsUnra9fhQ6C0x7G92y-iLTped6omk9DRZuiGApA",
+
+      "x-connector-refresh-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VybmFtZSI6InJpYzAiLCJlbWFpbCI6ImFkbWluQHRlc3QuaXQiLCJyb2xlcyI6WyJBRE1JTiIsIlVTRVIiXSwicGVybWlzc2lvbnMiOlsicmVhZDpjb21tZW50cyIsInJlYWQ6cHJvZmlsZSIsInJlYWQ6Y29tbWVudHMiXX0sImlhdCI6MTU0NjQ0MjgxNSwiZXhwIjoxNTQ3MDQ3NjE1fQ.-CvuWGYiam872qLZxDkREmvaGyufrKN37431Pj_6BAU"
+  }
+  </code>
+
 
 
 ## Additional:
--- backend/src/mocks/_users.js
+++ backend/src/mocks/_users.js
@@ -11,7 +11,15 @@ export const mockUsers = [
     name: 'Enrico',
     username: 'ric0',
     email: 'admin@test.it',
-    password: '$2a$12$1e616OUCfSM7Wd3VOvbZve.4DtCrRDPrAZcKvIo3.lDUHm3kiXhna', // this is === 123456
+    password: '$2a$12$1e616OUCfSM7Wd3VOvbZve.4DtCrRDPrAZcKvIo3.lDUHm3kiXhna', // this is === 123456 client-side crypto-hashed
+    // Note, to make login queries work in the graphql playground:
+    // * use 8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92 as 123456 client-side crypto-hashed in graphql plaground
+    // After that, to make _checkAuth and _currentUser work, use these headers:
+    // { 
+    //   "x-connector-auth-request-type": "LOCAL_STORAGE",
+    //   "x-connector-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VybmFtZSI6InJpYzAiLCJlbWFpbCI6ImFkbWluQHRlc3QuaXQiLCJyb2xlcyI6WyJBRE1JTiIsIlVTRVIiXSwicGVybWlzc2lvbnMiOlsicmVhZDpjb21tZW50cyIsInJlYWQ6cHJvZmlsZSIsInJlYWQ6Y29tbWVudHMiXX0sImlhdCI6MTU0NjQ0MjgxNSwiZXhwIjoxNTQ2NDQzMTc1fQ.EEcCsUnra9fhQ6C0x7G92y-iLTped6omk9DRZuiGApA",
+    //   "x-connector-refresh-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJ1c2VybmFtZSI6InJpYzAiLCJlbWFpbCI6ImFkbWluQHRlc3QuaXQiLCJyb2xlcyI6WyJBRE1JTiIsIlVTRVIiXSwicGVybWlzc2lvbnMiOlsicmVhZDpjb21tZW50cyIsInJlYWQ6cHJvZmlsZSIsInJlYWQ6Y29tbWVudHMiXX0sImlhdCI6MTU0NjQ0MjgxNSwiZXhwIjoxNTQ3MDQ3NjE1fQ.-CvuWGYiam872qLZxDkREmvaGyufrKN37431Pj_6BAU"
+    // }
     roles: [
       ROLES_PERMISSIONS.ADMIN.NAME,
       ROLES_PERMISSIONS.USER.NAME,
