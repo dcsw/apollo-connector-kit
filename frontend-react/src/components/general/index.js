e5e53cff957f6ef4c05a72c113dfcfeb29e8d5de
DeanC <>
2019-03-06 11:27:12 -0500

    Convert AdminBoundary to use recompose branch.

--- frontend-react/src/components/general/index.js
+++ frontend-react/src/components/general/index.js
@@ -1,5 +1,5 @@
 export { default as ErrorPage } from './_error-pages';
 export { default as Authenticate } from './_authentication';
-export { default as AdminBoundary } from './_admin';
+export { default as AdminBoundary } from './_admin-boundary';
 export { default as Loading } from './_loading';
 export { default as ErrorBoundary } from './_error-boundary';
